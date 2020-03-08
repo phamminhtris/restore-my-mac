dbb9f8e879091cca9fe17de161f775d1b886f8ae
Tri Pham <phamminhtris@tamu.edu>
2020-03-07 22:20:28 -0600

    More casks

--- install_cask.sh
+++ install_cask.sh
@@ -5,7 +5,7 @@ brew_packages=(appcleaner bartender caprine docker \
 	figma firefox google-chrome iterm2 swiftformat-for-xcode \
 	visual-studio-code zoomus \
 	homebrew/cask-fonts/font-fira-code \
-	swish)
+	swish macs-fan-control iina logitech-options)
 for package in "${brew_packages[@]}"
 do
 	brew cask install $package
